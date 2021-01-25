e4abf2da08b07c6b50396a138ed455a2fcd5f9f1
davidx315 <david315cg@hotmail.com>
2021-01-25 12:13:24 -0500

    Se crea la tabla para listar los heroes

--- js/script.js
+++ js/script.js
@@ -1,64 +1,69 @@
-const ngForFunctionality = async () => {
+const implementarHeroes = async () => {
     const heroes = await fetch('https://ddragon.leagueoflegends.com/cdn/11.2.1/data/es_MX/champion.json');
     let listadoHeroes = await heroes.json();
     const listadoHeroesArray = Object.values(listadoHeroes.data);
-
-    for (let index = 0; index < listadoHeroesArray.length; index++) {
-        const element = listadoHeroesArray[index];
-        //console.log(element);
-    }
-
-    let value = '';
-    let cont = 0;
+    let filas = ''
+    let columnas = '';
+    let contadorGrupo = 0;
+    let contadorGeneral = 0;
     listadoHeroesArray.forEach((post) => {
-        cont++;
-        value += `
+        contadorGeneral++;
+        contadorGrupo++;
+        columnas += `
+        <td>
             <div class="container-heroes" >
                 <img  class="imgRespuesta" src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${post.id}_0.jpg" alt="">
                 <div class="container-heroes-descripcion">
                     <h2  class="tituloRespuesta">${post.id}</h2>
                     <p  class="parrafoRespuesta">${post.blurb}</p>
                     <br/>
-                    <button class="boton-heroes" id="btnModal${cont.toString()}">Saber Mas</button>
+                    <button class="boton-heroes" id="btnModal${contadorGeneral.toString()}">Saber Mas</button>
                 </div>
             </div>
             <br/>
-            <div id="tvesModal${cont.toString()}" class="modalContainer">
+            <div id="tvesModal${contadorGeneral.toString()}" class="modalContainer">
                 <div class="modal-content">
-                    <img  class="" src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${post.id}_0.jpg" alt="">
-                    <span class="close"></span>        
-                    <div class="container-heroes-descripcion">
-                        <h2  class="tituloRespuesta">${post.id}</h2>
-                        <p  class="parrafoRespuesta">${post.blurb}</p>
-                        <br>
-                        <p class="parrafoRespuesta">${post.tags}</p>
+                    <img  class="imgModal" src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${post.id}_0.jpg" alt="">
+                    <span class="close"></span>  
+                         
+                    <div class="descripcion-modal">
+                        <button class="boton-cerrar" id="btnCerrar${contadorGeneral.toString()}"><strong>X</strong></button>  
+                        <h2  class="">${post.id}</h2>
+                        <h3  class="">${post.title}</h3>
+                        <p  class="">${post.blurb}</p>
+                        <p class="">${post.tags}</p>                  
                         <br/>
-                        <button>Saber Mas</button>
                     </div>
                 </div>
             </div> 
+            </td>
         `;
+        if (contadorGrupo === 4){
+            contadorGrupo = 0;
+            filas = filas +`<table class="tabla-heroes"><tr>`+columnas+`</tr></table>`
+            columnas = ``;
+        }else if(contadorGeneral === listadoHeroesArray.length)
+        {
+            filas = filas +`<table><tr>`+columnas+`</tr></table>`
+        }
     });
-
-    document.getElementById("myP").innerHTML = value;
+    document.getElementById("myHeroes").innerHTML =`<table>`+ filas +`</table>`;
     asinarClickModal();   
 };
+ 
 
 const asinarClickModal = () =>{
 
     let botones = document.getElementsByClassName("boton-heroes");
     let modales = document.getElementsByClassName("modalContainer");
+    let botonesCerrar = document.getElementsByClassName("boton-cerrar");
     
-    console.log(botones);
-
     for (let index = 0; index < botones.length; index++) {
-        var btn     = botones[index];
-        var modal   = modales[index];
-        var span    = document.getElementsByClassName("close")[0];
+        let btn     = botones[index];
+        let modal   = modales[index];
+        let botonesC  = botonesCerrar[index];
         var body    = document.getElementsByTagName("body")[0];
-        
-        console.log(modal);
-    
+          
         btn.onclick = function() {
             
             modal.style.display = "block"; 
@@ -66,30 +71,16 @@ const asinarClickModal = () =>{
             body.style.height = "100%";
             body.style.overflow = "hidden";
         }
-    
-        span.onclick = function() {
-            modal.style.display = "none";
-    
+
+        botonesC.onclick = function() {
+            
+            modal.style.display = "none";  
             body.style.position = "inherit";
             body.style.height = "auto";
             body.style.overflow = "visible";
-        }
-    
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-    
-                body.style.position = "inherit";
-                body.style.height = "auto";
-                body.style.overflow = "visible";
-            }
-        }    
+        }        
     }
-
-
-
 };
-
-ngForFunctionality();
+implementarHeroes();
 
 
